f9e98dc5d0c40ce3f4d0d703f6d873ec2f86a7e8
Martin Ortega <lynxestudio@outlook.com>
2023-12-04 14:55:06 -0600

    Add source code to the project.

---
+++ src/TestPgHelperProducts1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace TestPgHelperProducts1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine(Environment.NewLine);
+            Console.Write(" Enter product's code: ");
+            string code = Console.ReadLine();
+            Console.Write(" Enter product's name: ");
+            string name = Console.ReadLine();
+            string msg = ProductHelper.CreateProduct(code, name);
+            Console.WriteLine("\n============= OUTPUT===========\n");
+            Console.WriteLine(msg);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("\nPress <ENTER> to continue");
+            Console.ReadLine();
+        }
+    }
+}
