a99aa112831cd943837b2f3297d2b9162674865e
Martin A. Ortega <lynxestudio@outlook.com>
2024-11-25 10:09:52 -0600

    Modifies create_sript removes 2 additional columns

--- src/scripts/create_script.sql
+++ src/scripts/create_script.sql
@@ -3,7 +3,5 @@ create table products
 	id serial not null
 	,product_code varchar(16) primary key unique
 	,product_name varchar(128) not null
-	,blocked boolean default 'f'
 	,created timestamp null default now()
-	,modified timestamp null default now()
 );
